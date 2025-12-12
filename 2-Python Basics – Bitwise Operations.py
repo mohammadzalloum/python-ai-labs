a103b80f1a2874468d161d9e0b5c41bc6dc34748
mohammad zalloum <75264696+mohammadzalloum@users.noreply.github.com>
2025-12-12 22:05:01 +0300

    Add files via upload

---
+++ 2-Python Basics – Bitwise Operations.py
@@ -0,0 +1,20 @@
+print("Requirement 1:")
+age = 17
+name = "moh"
+if age <= 18 and name == "moh":
+    print("this is the right gay")
+elif age >19 and name != "moh":
+    print("not you")
+
+
+print("Requirement 1:")
+
+print(bin(5) and bin(6)==0)
+
+def naum(number):
+    if number == 5 or number <5:
+        return("number is 5 or less")
+    elif number != 5 and number >5:
+        return("number is bigger or not equal 5")
+
+print(naum(int(input("enter number"))))
